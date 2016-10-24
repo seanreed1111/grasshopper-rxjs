5bb6edb45ca52e5f28b91da34e5e9856929a8e6e
seanreed1111 <seanreed1111@gmail.com>
2016-10-24 18:04:05 -0400

    finshes version 1.0

--- bundle.js
+++ bundle.js
@@ -1,29 +1,19 @@
 (function() {
 
   var startButton = document.getElementById("startButton");
-  var endButton = document.getElementById("endButton");
+
   var board = document.getElementById("board");
   var squares = document.querySelectorAll(".square")
 
   var winningBoardString = JSON.stringify(["R","R","R","R","","L","L","L","L"]);
 
   var startButtonClick$ = Rx.Observable.fromEvent(startButton, 'click').first();
-  var endButtonClick$ = Rx.Observable.fromEvent(endButton, 'click').first();
+
   var boardClick$ = Rx.Observable.fromEvent(board, 'click');
 
-  startButtonClick$.subscribe(
-    function(e) {console.log("Start Button Clicked!")},
-    function(err) {console.log("err:" +err)},
-    function() {console.log("startButtonClick$ completed")}
-  );
 
-  endButtonClick$.subscribe(
-    function(e) {console.log("End Button Clicked!")},
-    function(err) {console.log("err:" +err)},
-    function() {console.log("endButtonClick$ completed")}
-  );
-
-  var runGame$ = boardClick$
+  var runGame$ = startButtonClick$
+  .switchMap(function(e) {return boardClick$})
   .map(function(e) {return e.target.dataset.location})
   .filter(function(location) {return location != undefined})
   .scan(
@@ -41,31 +31,16 @@
 
   var currentBoard$ = runGame$
   .map(function(acc) {return JSON.stringify(acc.boardArray)})
-  .takeWhile(function(currentBoardString) {return currentBoardString !== winningBoardString})
+  .filter(function(currentBoardString){return currentBoardString === winningBoardString})
+  .do(function(isWinner) { // side effect
+    if(isWinner) {gameOver();}
+  })
 
-
-  // in words:
-  // The current board is JSON.stringify(boardArray) 
-
-  // Continue to run the game until currentBoard$ emits the winningBoardString
-
-  // then, 
-  //  stop listening, 
-  // tell the player they've won the game
-  // and end the game
-
-
-
-
-
-  // runGame$.subscribe(
-  //   function(x){console.log("from runGame$ boardArray: " +x.boardArray);}
-  //   );
 
   currentBoard$.subscribe(
     function(x){console.log("from currentBoard$ boardString: " +x);},
     function(err) {console.log('err: '+err)},
-    function() {console.log('currentBoard completed!')}
+    function() {console.log('congratulations! You finished the game!')}
     );
 
   function isValidMove(indexOfBlank, location){
@@ -84,5 +59,16 @@
   function updateUI(boardArray){
     squares.forEach(function(square) {square.innerHTML = boardArray[square.dataset.location];})
   }
-  
+
+  function gameOver(){
+      console.log("WINNER!!!!!!!!!!!")
+      h1 = document.createElement("h1");
+      text1 = document.createTextNode("You Won!! Congratulations!");
+      text2 = document.createTextNode("Press Cmd-R to Play Again");
+
+      winner = document.getElementById("winner")
+      winner.appendChild(h1).appendChild(text1);
+      winner.appendChild(text2);
+  }
+
 })(); 
